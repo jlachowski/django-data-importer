9b70a85a083d5470ad48581e197273279807288e
Jaroslaw Lachowski <jaroslaw.lachowski@wilabs.com>
2015-06-26 15:19:42 +0200

    future fixes & version bump

--- data_importer/__init__.py
+++ data_importer/__init__.py
@@ -1,13 +1,5 @@
 # coding: utf-8
-from __future__ import absolute_import
-from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from future import standard_library
-standard_library.install_aliases()
-from builtins import str
-from builtins import *
-VERSION = (0, 1, 4)
+VERSION = (0, 1, 5)
 
 
 def get_version():
